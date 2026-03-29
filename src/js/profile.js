b6030ee861f6910a0ce2f2d133adb6b3e7f37e0c
stacelita <gardensea.lw@gmail.com>
2026-03-29 14:44:38 +0900

    スタッフ情報を取得

--- src/js/profile.js
+++ src/js/profile.js
@@ -20,10 +20,22 @@ async function setupProfilePage() {
     initializeBirthDateSelectors();
 
     const profile = await liff.getProfile();
-    
+    const data = await apiGet(ACTION.GET_PROFILE, {
+      userId: profile.userId,
+    });
 
     // データが存在する場合のみフォームにセット
- 
+    if (data) {
+      // formのname属性やid属性に合わせて調整してください
+      getEl("userName").value = data.name || '';
+      getEl("userKana").value = data.furigana || '';
+      getEl("station").value = data.station || '';
+      getEl("tel").value = data.phone || '';
+      const [year, month, day] = data.birthday.split('-');
+      getEl("birthYear").value = Number(year);
+      getEl("birthMonth").value = Number(month);
+      getEl("birthDay").value = Number(day);
+    }
 
     setOverlay(false);
   } catch (error) {
